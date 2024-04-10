4561ed234aecb9256995b433eaaa4bd62b3b56be
220520sa <yamilfernado88@gmail.com>
2024-04-10 10:56:07 -0300

    creacion del navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProductosComponent } from './productos/productos.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ProductosComponent,
+    AcercadeComponent,
+    GaleriaComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
